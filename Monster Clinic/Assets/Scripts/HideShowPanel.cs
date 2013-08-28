46416a8b86345c2af8f8edb7f8e910e5809d5398
mark pollard <mark@marks-iMac.local>
2013-08-28 15:37:27 +0100

    Added very basic show and hide button

---
+++ Monster Clinic/Assets/Scripts/HideShowPanel.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideShowPanel : MonoBehaviour {
+	
+	public bool isOpen = true; 
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Toggle()
+	{
+		if(isOpen)
+		{
+			Hide();
+			isOpen = !isOpen;
+		}
+		else
+		{
+			Show ();
+			isOpen = !isOpen;
+		}
+	}
+	
+	
+	void Hide()
+	{
+		/// hide the panel	
+		
+		Animation anim = GetComponent<Animation>();
+		anim.animation["Close"].speed = 1;
+		anim.Play("Close");
+	}
+	
+	void Show()
+	{
+		/// show the panel
+		Animation anim = GetComponent<Animation>();
+		anim.animation["Close"].speed = -1;
+		anim.animation["Close"].time = anim.animation["Close"].length;
+		anim.animation.Play("Close");
+	}
+}
---
+++ Monster Clinic/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		///check which mode we are in to show the gui
+		
+	}
+}
