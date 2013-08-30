28fdf9821a62d0d611bae8646b5e5e512644f461
mark pollard <mark@marks-iMac.local>
2013-08-30 15:35:41 +0100

    Added the right menu for the items

---
+++ Monster Clinic/Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Item {
+
+	private string _name;
+	private string _description;
+	private int _cost;
+	private Vector2 _position;
+	private int _sellPrice;
+}
