5001225ff4ebdf85d53dc25effb313fc9f00dd3f
mark pollard <mark@marks-iMac.local>
2013-08-31 10:57:17 +0100

    Created a item list class for item buttons and objects

--- Monster Clinic/Assets/Scripts/Item.cs
+++ Monster Clinic/Assets/Scripts/Item.cs
@@ -2,11 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Item {
+public class Item : MonoBehaviour {
 
-	private string _name;
-	private string _description;
-	private int _cost;
+	public string itemName;
+	public string description;
+	public int cost;
+	public GameObject placeableObject;
+	public RoomType roomType;
+	
+    public UILabel itemNameLabel;
+	public UILabel itemCostLabel;
+	public UILabel itemDescLabel;
+	
 	private Vector2 _position;
 	private int _sellPrice;
+	
+	void Start()
+	{
+		itemNameLabel.text = itemName;	
+		itemCostLabel.text = cost.ToString();
+		itemDescLabel.text = description;
+	}
 }
