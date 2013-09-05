41a0f203c0b7e080651900b0f91dd82a601dca69
mark pollard <mark@marks-iMac.local>
2013-09-06 00:15:14 +0100

    Added the staff class

---
+++ Monster Clinic/Assets/Scripts/Prefabs.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Prefabs : MonoBehaviour {
+	//static class for prefabs
+	
+	public static GameObject Octodoctor;
+	public static GameObject Cthuluburse;
+	public static GameObject Yetitor;
+}
---
+++ Monster Clinic/Assets/Scripts/StaffManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class StaffManager : MonoBehaviour {
+	
+	public GameObject Octodoctor;
+	public GameObject Cthuluburse;
+	public GameObject Yetitor;
+	
+	public List<Staff> staffList = new List<Staff>();
+	
+	
+	// Use this for initialization
+	void Start () {
+		///ref the gameobjects to the prefab class
+		Prefabs.Octodoctor = Octodoctor;
+		Prefabs.Cthuluburse = Cthuluburse;
+		Prefabs.Yetitor = Yetitor;
+	}
+	
+	
+	void OnGUI()
+	{
+		//just for a test buttons
+		if(GUI.Button(new Rect(0,50,100,20),"Octodoctor"))
+		{
+			Staff staff = new Staff( StaffType.Octodoctor, "This is a Doctor");	
+			staffList.Add(staff);
+		}
+		if(GUI.Button(new Rect(150,50,100,20),"Cthuluburse"))
+		{
+			Staff staff = new Staff( StaffType.Cthuluburse, "This is a nurse");	
+			staffList.Add(staff);
+		}
+		if(GUI.Button(new Rect(300,50,100,20),"Yetitor"))
+		{
+			Staff staff = new Staff( StaffType.Yetitor, "This is a engineer");	
+			staffList.Add(staff);
+		}
+	}
+}
